bb7cfdba657a7e1d356b43b9d920600ea31914d9
Ricardo Ferreira <j.ricardo.ferreira@hotmail.com>
2024-04-12 23:40:47 -0400

    Primeiro commit com projeto Electron

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Configure live reload for Electron
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+function createWindow() {
+    // Cria uma janela de navegação.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true, // Ativar integração Node.js no processo de renderização
+            contextIsolation: false, // Configurações importantes para este projeto (altere para produção!)
+            enableRemoteModule: true, // Permite o uso do módulo remoto, se necessário
+        }
+    });
+
+    // Carrega o arquivo index.html da aplicação.
+    mainWindow.loadFile('index.html');
+
+    // Abre o DevTools.
+    // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ renderer.js
@@ -0,0 +1,75 @@
+const { exec } = require('child_process');
+
+function updateStatusIndicator(isConnected) {
+    const statusElement = document.getElementById('status');
+    statusElement.style.backgroundColor = isConnected ? 'green' : 'red';
+}
+
+function checkDeviceConnection() {
+    exec('adb devices', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            updateStatusIndicator(false);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            updateStatusIndicator(false);
+            return;
+        }
+
+        const isConnected = stdout.split('\n').some(line => line.trim().endsWith('device'));
+        updateStatusIndicator(isConnected);
+    });
+}
+
+function getAndroidProperties() {
+    exec('adb shell getprop', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            updateStatusIndicator(false);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            updateStatusIndicator(false);
+            return;
+        }
+
+        const propertyMap = {
+            'ro.build.fingerprint': 'Fingerprint',
+            'ro.build.id': 'Build Version',
+            'ro.build.product': 'Product',
+            'ro.build.type': 'Build Type',
+            'ro.build.version.release': 'Android Version',
+            'ro.build.version.sdk': 'SDK Version',
+            'ro.carrier': 'Channel',
+            'ro.boot.hardware.sku': 'SKU',
+            'ro.vendor.product.name': 'Build Target'
+        };
+
+        const properties = stdout.split('\n')
+          .filter(line => line.match(/^\[ro\.(build\.(fingerprint|id|product|type|version\.release|version\.sdk)|carrier|boot\.hardware\.sku|vendor\.product\.name)\]: \[(.*)\]/))
+          .map(prop => {
+            const match = prop.match(/^\[([^\]]+)\]: \[([^\]]+)\]/);
+            const propertyName = match[1];
+            const propertyValue = match[2];
+            const displayName = propertyMap[propertyName] || propertyName;
+            return `<span class="property-name">${displayName}:</span> ${propertyValue}`;
+        });
+
+        // Exibir as propriedades na página
+        document.getElementById('deviceInfo').innerHTML = properties.join('<br>');
+        updateStatusIndicator(true);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const refreshButton = document.getElementById('refreshButton');
+    refreshButton.addEventListener('click', () => {
+        checkDeviceConnection();
+        getAndroidProperties();
+    });
+
+    setInterval(checkDeviceConnection, 5000);
+});
